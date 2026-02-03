d351ccbf7acafddb1dc91ac0ffb2e661053af48a
Julien Conoir <julien.conoir.etu@univ-lille.fr>
2026-02-03 16:43:25 +0100

    test: add setup to integration tests for PrismaWebinarRepository

--- src/webinars/adapters/webinar-repository.prisma.int.test.ts
+++ src/webinars/adapters/webinar-repository.prisma.int.test.ts
@@ -1,2 +1,44 @@
-// Test d'intégration
-// C. Ecriture de notre premier test d'intégration
+import { PrismaClient } from '@prisma/client';
+import { PostgreSqlContainer, StartedPostgreSqlContainer } from '@testcontainers/postgresql';
+import { exec } from 'child_process';
+import { PrismaWebinarRepository } from 'src/webinars/adapters/webinar-repository.prisma';
+import { Webinar } from 'src/webinars/entities/webinar.entity';
+import { promisify } from 'util';
+const asyncExec = promisify(exec);
+
+describe('PrismaWebinarRepository', () => {
+  let container: StartedPostgreSqlContainer;
+  let prismaClient: PrismaClient;
+  let repository: PrismaWebinarRepository;
+
+  beforeAll(async () => {
+    container = await new PostgreSqlContainer()
+      .withDatabase('test_db')
+      .withUsername('user_test')
+      .withPassword('password_test')
+      .withExposedPorts(5432)
+      .start();
+
+    const dbUrl = container.getConnectionUri();
+    prismaClient = new PrismaClient({
+      datasources: {
+        db: { url: dbUrl },
+      },
+    });
+
+    await asyncExec(`DATABASE_URL=${dbUrl} npx prisma migrate deploy`);
+
+    return prismaClient.$connect();
+  });
+
+  beforeEach(async () => {
+    repository = new PrismaWebinarRepository(prismaClient);
+    await prismaClient.webinar.deleteMany();
+    await prismaClient.$executeRawUnsafe('DELETE FROM "Webinar" CASCADE');
+  });
+
+  afterAll(async () => {
+    await container.stop({ timeout: 1000 });
+    return prismaClient.$disconnect();
+  });
+});
